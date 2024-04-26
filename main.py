9b2b54493b6dcc008fd48abe5902e5569dd3a9c5
Astruso <167698323+Astruso@users.noreply.github.com>
2024-04-26 18:13:49 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print("This program is a simple calcuator that will multiply two numbers together")
+first_num=int (input("please enter your first number here  "))
+second_num=int (input("please enter your second number here   "))
+total = first_num * second_num
+print (f"the product of {first_num} * {second_num} = {total}")
